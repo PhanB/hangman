e5191849d9db9dd689fe9ed3dc939e41ed4c6179
Bailey Phan <bailey.phan@uky.edu>
2017-08-18 02:05:26 -0700

    Reprint game over itself, track user guesses, and filter input.
    Using ANSI escape codes we can move our cursor up/down the stdout,
    allowing us to reprint over our previously printed lines.
    User's previous guesses are tracked in a list and printed to screen.
    User input is now filtered by length > 1 and regex is used to determine
    if the input is composed of characters in the alphabet.

--- hangman.py
+++ hangman.py
@@ -6,14 +6,20 @@ Purpose: Simple recreation of hangman in Python
 import random 
 import ascii
 import fonts
+import re
 
 MAX_LIVES = len(ascii.hangman) - 1
+LINES_PER_GAME = 11
 
 def main():
+	for i in range(0,LINES_PER_GAME):
+		print()
 	keepPlaying = 'y'
 	while(keepPlaying == 'y' or keepPlaying == 'yes'):
 		playRound()
 		keepPlaying = input('Would you like to keep playing (y/n)?: ')
+		moveCursorUp(1)
+		print('',)
 	print("Thank you for playing!")
 
 
@@ -24,6 +30,7 @@ def playRound():
 	answer = list(retrieveWord()) #convert string to list so it is mutable
 	revealed = []
 	char_dict = {} #holds characters and their position(s) in string
+	user_guesses = [] #all of user's guesses
 
 	#parse word, fill out dictionary, create string that holds revealed chars
 	for i in range(0,len(answer)-1):
@@ -34,26 +41,39 @@ def playRound():
 			char_dict[answer[i]] = [i]
 
 	#game loop -- exit conditions are out of lives or they guessed the word
-	printHangman(spaceRevealed(revealed),lifeCount[0])
+	printHangman(spaceRevealed(revealed), lifeCount[0])
 	while(lifeCount[0] > 0 and not guessed):
 		#printRevealed(revealed)
 		print('****************************************')
-		guessed = guess(char_dict, revealed, lifeCount)
-		printHangman(spaceRevealed(revealed),lifeCount[0])
+		guessed = guess(char_dict, revealed, lifeCount, user_guesses)
+		printHangman(spaceRevealed(revealed), lifeCount[0])
 
 
 	if(guessed): #they guessed the answer
-		print("You win! You guessed the word: " + ''.join(answer))
+		printCustomize([fonts.GREEN, fonts.BOLD], "You win! You guessed the word: " + ''.join(answer))
 	else: #they didnt guess the answer
-		print("You lose! The word was: " + ''.join(answer))
+		printCustomize([fonts.RED, fonts.BOLD], "You lose! The word was: " + ''.join(answer))
 	
 def retrieveWord():
 	lines = open("wordbank.txt").readlines()
 	return random.choice(lines)
 	
-def guess(dictionary, revealed, lives):
+def guess(dictionary, revealed, lives, guesses):
 
-	user_guess = input('Guess a character: ')
+	print("Charcters guessed: " + str(guesses))
+	
+	#get user guess, but make sure they enter a valid character
+	user_guess = ''
+	while(True):
+		user_guess = input('Guess a character: ')
+		if(len(user_guess) >= 1 and re.match('^[a-zA-Z]+$', user_guess)):
+			user_guess = user_guess[0]
+			break
+		else:
+			moveCursorUp(1)
+			clearCurrentLine()
+	
+	guesses.append(user_guess)
 	if user_guess in dictionary: #they guessed correctly
 		#reveal the characters
 		for i in dictionary[user_guess]:
@@ -62,7 +82,7 @@ def guess(dictionary, revealed, lives):
 		#remove entry from dictionary
 		del dictionary[user_guess]
 
-		printCustomize([fonts.GREEN,fonts.BOLD],"CORRECT!")
+		#printCustomize([fonts.GREEN,fonts.BOLD],"CORRECT!")
 
 		#if dictionary is empty, they win
 		if(not bool(dictionary)):
@@ -71,8 +91,7 @@ def guess(dictionary, revealed, lives):
 			return False
 	else: #they guessed incorrectly
 		lives[0]-= 1;
-		#print(fonts.RED + fonts.BOLD + "Incorrect!" + fonts.ENDC)
-		printCustomize([fonts.RED,fonts.BOLD], "INCORRECT!")
+		#printCustomize([fonts.RED,fonts.BOLD], "INCORRECT!")
 		return False
 
 def spaceRevealed(word):
@@ -81,10 +100,15 @@ def spaceRevealed(word):
 		sb.append(c + " ")
 	return ''.join(sb)
 
+#printing functions
 def printHangman(revealedChars, lives):
 	hangman_info = ascii.hangman[MAX_LIVES-lives].splitlines()
 	halfway = int(len(hangman_info) / 2)
 	hangman_info[halfway]+= '\t\t' + fonts.BOLD + revealedChars + fonts.ENDC
+
+	#move cursor up (rewrite over to save space in console)
+	moveCursorUp(LINES_PER_GAME)
+
 	for line in hangman_info:
 		print(line)
 			
@@ -100,8 +124,15 @@ def	printCustomize(customizations, text):
 
 	print(''.join(sb))
 
+#cursor manipulation functions
+def moveCursorUp(linesUp):
+	#move up while clearing out each line
+	for i in range(0,linesUp):
+		print("\033[F", end="")
+		clearCurrentLine()
 
-
+def clearCurrentLine():
+	print("\033[K", end='') # Clear to the end of line
 
 
 if __name__ == "__main__":
